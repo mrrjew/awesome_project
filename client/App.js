192c9d7a22f451a29bff8e72bf357c989c2201c3
mrr.jew <100996700+mrrjew@users.noreply.github.com>
2024-05-11 14:40:20 +0000

    refactor(googleapis) Enabled apis

--- client/App.js
+++ client/App.js
@@ -1,21 +1,37 @@
-import { StyleSheet} from 'react-native';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import HomeScreen from './screens/HomeScreen';
+import { Provider } from "react-redux";
+import { store } from "./store";
+import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
     <Provider store={store}>
-      <HomeScreen/>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              options={{
+                headerShown: false,
+              }}
+              name="HomeScreen"
+              component={HomeScreen}
+            />
+            <Stack.Screen
+              options={{
+                headerShown: false,
+              }}
+              name="MapScreen"
+              component={MapScreen}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
